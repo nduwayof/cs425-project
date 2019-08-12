073c34e28ccbafecc8fc28e46431efc153ebf6ef
Nduwayo Fabrice <nduwayof@outlook.com>
2019-08-12 16:23:44 -0500

    Initialized the git repository

---
+++ src/main/java/com/ugandaairlines/ugair/UgandanAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.ugandaairlines.ugair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UgandanAirlinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UgandanAirlinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ugandaairlines/ugair/FlyWithUgairApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ugandaairlines.ugair;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlyWithUgairApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
